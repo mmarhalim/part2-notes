cae794e0905f35adcfe385f005e4786a9d641d81
mmarhalim <115380281+mmarhalim@users.noreply.github.com>
2023-03-18 12:21:19 -0600

    added axios dependency

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Note from './components/Note'
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes)
+
+const App = () => {
+  const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
 
